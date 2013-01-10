a326a7efcab7ad331b1fee6050235544766093a8
Pedro Fernandez M <pedrofernandezm@gmail.com>
2013-01-09 22:47:38 -0500

    creating app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoccerTeamManager::Application.config.secret_token = 'e180ecc46c719e96236618574f1ca04f2f048777ac4ece785b8b33c011809807a930131ef2202844cf9c8fc8358f5ca6344883061e2bca115284f7aee3b37fd0'
